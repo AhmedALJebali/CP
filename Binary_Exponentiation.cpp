84056b5a69ef911662969424c5573d22e66cd42a
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2025-12-14 07:17:07 +0200

    Implement binary exponentiation function

---
+++ Binary_Exponentiation.cpp
@@ -0,0 +1,11 @@
+int binpow(int a, int b, int m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
