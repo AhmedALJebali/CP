916bb1cf0c19074d7f319fb850be13446249fcca
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2025-12-18 06:28:14 +0200

    Refactor ordered multiset into a structured class
    Refactor ordered multiset implementation to encapsulate functionality within a struct. Added methods for insertion, deletion, range counting, and bounds retrieval.

--- ordered_multiset.cpp
+++ ordered_multiset.cpp
@@ -1,20 +1,53 @@
+
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<
-    pair<int,int>,
-    null_type,
-    less<pair<int,int> >,
-    rb_tree_tag,
-    tree_order_statistics_node_update
-> ordered_multiset;
+using namespace std;
+template<typename key>
+using ordered_multiset = tree<key, null_type,less_equal/*can do greater and less_equal*/<key>,rb_tree_tag,tree_order_statistics_node_update>; /* set.order_of_key(x) => give you number of elemenst smallest than x in the set && *set.find_by_order(x) give you the elemnt in the xth index in the set both on log(N)*/
 
-ordered_multiset st;
-int timer = 0;
+struct orderedMuiltiset {
+    ordered_multiset<int> se;
 
-void insert_value(int x) {
-    st.insert({x, timer++});
-}
+    void insert(int val) { se.insert(val); }
 
-int count_range(int l, int r) {
-    return st.order_of_key({r + 1, 0}) - st.order_of_key({l, 0});
-}
+    bool exist(int val) {
+        if (se.upper_bound(val) == se.end()) { return false; }
+        return (*se.upper_bound(val)) == val;
+    }
+
+    void erase1(int val) { if (exist(val)) { se.erase(se.upper_bound(val)); }}
+
+    void erase(int val) {
+        auto it = se.find(val);
+        if (it != se.end()) { se.erase(it); }
+    }
+
+    void erase_one(int val) {
+        auto it = se.lower_bound(val);
+        if (it != se.end() && *it == val) { se.erase(it); }
+    }
+
+    int index(int val) {
+        if (!exist(val)) { return -1; }
+        return se.order_of_key(val);
+    }
+
+    int LastIdx(int val) {
+        if (!exist(val)) { return -1; }
+        if (*se.find_by_order(se.size() - 1) == val) { return se.size() - 1; }
+        return index(*se.lower_bound(val)) - 1;
+    }
+
+    int lower_bound(int val) { return *se.upper_bound(val); }
+
+    int upper_bound(int val) { return *se.lower_bound(val); }
+
+    int operator[](int idx) { return *se.find_by_order(idx); }
+
+    int size() { return se.size(); }
+
+    void clear() { se.clear(); }
+
+    int order_of_key(int x) { return se.order_of_key(x); }
+};
