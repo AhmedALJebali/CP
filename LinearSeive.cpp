a595265b4d245e1f2550b442221427c04f00097f
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2025-12-25 21:19:11 +0200

    Refactor Linear Sieve algorithm
    Refactor linear sieve implementation for clarity and efficiency.

--- LinearSeive.cpp
+++ LinearSeive.cpp
@@ -1,16 +1,20 @@
-const int N = 10000000;
-vector<int> lp(N+1);
-vector<int> pr;
+const int N = 1e7;
 
-for (int i=2; i <= N; ++i) {
-    if (lp[i] == 0) {
-        lp[i] = i;
-        pr.push_back(i);
-    }
-    for (int j = 0; i * pr[j] <= N; ++j) {
-        lp[i * pr[j]] = pr[j];
-        if (pr[j] == lp[i]) {
-            break;
+int lp[N + 1];
+bool isPrime[N + 1];
+vector<int> primes;
+
+void linear_sieve(int n) {
+    for (int i = 2; i <= n; i++) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            isPrime[i] = true;
+            primes.push_back(i);
+        }
+        for (int p: primes) {
+            if (p > lp[i] || i * p > n) break;
+            lp[i * p] = p;
+            isPrime[i * p] = false;
         }
     }
 }
