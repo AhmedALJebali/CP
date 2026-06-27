cea8921ac8338604d46cd50f0ab4d89f09846fc6
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-06-27 16:31:57 +0300

    Refactor DynamicSegtree to use int instead of long long

--- Data structure/Segment Tree/DynamicSegtree.cpp
+++ Data structure/Segment Tree/DynamicSegtree.cpp
@@ -1,114 +1,123 @@
+const int MOD=1e9+7;
 struct Node {
-    long long sum, lazy;
+    int sum, lazy;
     int is_lazy;
-    int lc, rc; // Indices for left and right children
-
-    Node() { 
-        sum = lazy = is_lazy = 0; 
-        lc = rc = -1; 
+    int lc, rc;
+    Node() {
+        sum = lazy = is_lazy = 0;
+        lc = rc = -1;
     }
-
-    Node(long long x) {
+    Node(int x) {
         sum = x;
         lazy = is_lazy = 0;
         lc = rc = -1;
     }
-
-    void change(long long lx, long long rx, long long x) {
-        long long len = (rx - lx);
-        sum += (x * len);
-        lazy += x;
+    void change(int lx, int rx, int x) {
+        int len = (rx - lx) % MOD;
+        sum = (sum + (x % MOD) * len) % MOD;
+        lazy = (lazy + x) % MOD;
         is_lazy = 1;
     }
 };
 
 struct DynamicSagara {
-    long long min_val, max_val;
+    int min_val, max_val;
     vector<Node> segData;
     int root;
-    DynamicSagara(long long min_val, long long max_val) {
+
+    DynamicSagara(int min_val, int max_val) {
         this->min_val = min_val;
         this->max_val = max_val;
         root = create_node();
     }
+
     int create_node() {
         segData.push_back(Node());
         return segData.size() - 1;
     }
+
     Node merge(const Node& left, const Node& right) {
         Node res;
-        res.sum = left.sum + right.sum;
+        res.sum = (left.sum + right.sum)%MOD;
         return res;
     }
-    void propagate(int node, long long lx, long long rx) {
-        if (rx - lx == 1) return; 
-        // Dynamically create children if they don't exist
+
+    void propagate(int node, int lx, int rx) {
+        if (rx - lx == 1) return;
+
         if (segData[node].lc == -1) segData[node].lc = create_node();
         if (segData[node].rc == -1) segData[node].rc = create_node();
+
         if (segData[node].is_lazy == 0) return;
-        long long mid = lx + (rx - lx) / 2;
-        long long x = segData[node].lazy;
-        // Propagate to the dynamically created children
+
+        int mid = lx + (rx - lx) / 2;
+        int x = segData[node].lazy;
+
         segData[segData[node].lc].change(lx, mid, x);
         segData[segData[node].rc].change(mid, rx, x);
+
         segData[node].is_lazy = 0;
         segData[node].lazy = 0;
     }
 
-    void set(long long idx, long long val, int node, long long lx, long long rx) {
+    void set(int idx, int val, int node, int lx, int rx) {
         if (rx - lx == 1) {
-            segData[node].sum = val; // Update directly to preserve lc/rc as -1
+            segData[node].sum = val;
+            segData[node].lazy = 0;
+            segData[node].is_lazy = 0;
             return;
         }
         propagate(node, lx, rx);
-        long long mid = lx + (rx - lx) / 2;
-        
+        int mid = lx + (rx - lx) / 2;
+
         if (idx < mid) {
             set(idx, val, segData[node].lc, lx, mid);
         } else {
             set(idx, val, segData[node].rc, mid, rx);
         }
-        
+
         segData[node].sum = segData[segData[node].lc].sum + segData[segData[node].rc].sum;
     }
 
-    void set(long long idx, long long val) { 
-        set(idx, val, root, min_val, max_val); 
+    void set(int idx, int val) {
+        set(idx, val, root, min_val, max_val);
     }
 
-    void update(long long l, long long r, long long x, int node, long long lx, long long rx) {
+    void update(int l, int r, int x, int node, int lx, int rx) {
+        if (lx >= r || rx <= l) return;
         if (lx >= l && rx <= r) {
             segData[node].change(lx, rx, x);
             return;
         }
-        if (lx >= r || rx <= l) return;
-        
+
         propagate(node, lx, rx);
-        long long mid = lx + (rx - lx) / 2;
-        
+        int mid = lx + (rx - lx) / 2;
+
         update(l, r, x, segData[node].lc, lx, mid);
         update(l, r, x, segData[node].rc, mid, rx);
-        
+
         segData[node].sum = segData[segData[node].lc].sum + segData[segData[node].rc].sum;
     }
 
-    void update(long long l, long long r, long long x) { 
-        update(l, r, x, root, min_val, max_val); 
+    void update(int l, int r, int x) {
+        update(l, r, x, root, min_val, max_val);
     }
 
-    Node get(long long l, long long r, int node, long long lx, long long rx) {
+    Node get(int l, int r, int node, int lx, int rx) {
         if (node == -1 || lx >= r || rx <= l) return Node();
         if (lx >= l && rx <= r) {
             return segData[node];
         }
         propagate(node, lx, rx);
-        long long mid = lx + (rx - lx) / 2;
-        
+        int mid = lx + (rx - lx) / 2;
+
         Node left = get(l, r, segData[node].lc, lx, mid);
         Node right = get(l, r, segData[node].rc, mid, rx);
         return merge(left, right);
     }
-    Node get(long long l, long long r) { 
-        return get(l, r, root, min_val, max_val); 
+
+    Node get(int l, int r) {
+        return get(l, r, root, min_val, max_val);
     }
 };
+
