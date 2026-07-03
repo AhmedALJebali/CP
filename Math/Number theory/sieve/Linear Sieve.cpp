c6b2fdcde4e65992f7cb37ebe8b83ae00dae317b
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-07-03 17:01:37 +0300

    Refactor Linear Sieve function
    Refactor Linear Sieve function to remove return value and adjust formatting.

--- Math/Number theory/sieve/Linear Sieve.cpp
+++ Math/Number theory/sieve/Linear Sieve.cpp
@@ -1,15 +1,16 @@
+const int N=1e4+5;
 int Lpf[N];
-vector<int>LinearSieve(){
-    for(int i=1;i<N;i++)Lpf[i]=i;
-    vector<int>primes;
-    for(int i=2 ;i<N;i++){
-        if(Lpf[i]==i){
-            primes.emplace_back(i);
-        }
-        for(auto &val:primes){
-            if(i*val>=N)break;
-            Lpf[i*val] = val;
-        }
+vector<int>primes;
+void LinearSieve(){
+  for(int i=1;i<N;i++)Lpf[i]=i;
+  
+  for(int i=2 ;i<N;i++){
+    if(Lpf[i]==i){
+      primes.emplace_back(i);
     }
-    return primes;
+    for(auto &val:primes){
+      if(i*val>=N)break;
+      Lpf[i*val] = val;
+    }
+  }
 }
