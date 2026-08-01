2843b703fb095abd7740bf455bf0cca2af6a5847
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-01 16:26:43 +0300

    Update Primefactors.cpp

--- Math/Number theory/Primefactors.cpp
+++ Math/Number theory/Primefactors.cpp
@@ -1,12 +1,12 @@
 vector<int> PrimeFactors(int n) {
-  vector<int>ret;
-  for (int i = 2; i * i <= n; i = i + 2) {
+  vector<int> ret;
+  for (int i = 2; i * i <= n; i++) { 
     while (n % i == 0) {
       ret.push_back(i);
       n = n / i;
     }
   }
-  if (n > 2) {
+  if (n > 1) { 
     ret.push_back(n);
   }
   return ret;
