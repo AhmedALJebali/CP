f7e1391e50624718edf71ff5d68d7cdd25782a02
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-01 16:14:58 +0300

    Update all.cpp

--- Math/Number theory/theories/all.cpp
+++ Math/Number theory/theories/all.cpp
@@ -89,3 +89,12 @@ int Mfib(int n) {
 
     return Mn.b; // F_n
 }
+bool is_prime(int k) {
+  if (k <= 1) return false;
+  if (k <= 3) return true;
+  if (k % 2 == 0 || k % 3 == 0) return false;
+  for (int i = 5; i * i <= k; i += 6) {
+    if (k % i == 0 || k % (i + 2) == 0) return false;
+  }
+  return true;
+}
