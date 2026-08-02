280ecbe65586986f0a4d6503b1d8732845563333
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-02 06:12:36 +0300

    Implement Baby-step Giant-step algorithm with binpow

--- Math/Number theory/baby-step_giant-step.cpp
+++ Math/Number theory/baby-step_giant-step.cpp
@@ -28,3 +28,50 @@ int bsgs(int a, int b, int m) {
     }
     return -1;
 }
+
+// with min try both please
+int binpow(int a, int b, int m) {
+  int res = 1;
+  a %= m;
+  while (b > 0) {
+    if (b & 1) res = (res * a) % m;
+    a = (a * a) % m;
+    b >>= 1;
+  }
+  return res;
+}
+int bsgs(int a, int b, int m, int min_x = 0) {
+  a %= m, b %= m;
+  int k = 1, add = 0, g;
+  while ((g = __gcd(a, m)) > 1) {
+    if (b == k && add >= min_x)
+      return add;
+    if (b % g)
+      return -1;
+    b /= g, m /= g, ++add;
+    k = (k * 1ll * a / g) % m;
+  }
+  if (add < min_x) {
+    int req = min_x - add;
+    k = (k * binpow(a, req, m)) % m;
+    add = min_x;
+  }
+  int n = sqrt(m) + 1;
+  int an = 1;
+  for (int i = 0; i < n; ++i)
+    an = (an * 1ll * a) % m;
+  unordered_map<int, int> vals;
+  for (int q = 0, cur = b; q <= n; ++q) {
+    vals[cur] = q;
+    cur = (cur * 1ll * a) % m;
+  }
+  for (int p = 1, cur = k; p <= n; ++p) {
+    cur = (cur * 1ll * an) % m;
+    if (vals.count(cur)) {
+      int ans = n * p - vals[cur] + add;
+      return ans;
+    }
+  }
+  return -1;
+}
+
