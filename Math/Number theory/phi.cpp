6fe5d0833d48d3233034cd9a0431f3b0b74b6c2a
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-02 07:05:57 +0300

    Update phi.cpp

--- Math/Number theory/phi.cpp
+++ Math/Number theory/phi.cpp
@@ -13,19 +13,31 @@ int phi(int n) {
   return result;
 }
 // precomputes an array phi containing these counts for all numbers up to MAXN.
-const int MAXN = 1e6+5;
-int phi[MAXN];
-void totient_sieve() {
-    for (int i = 0; i < MAXN; i++) phi[i] = i;
-    for (int i = 2; i < MAXN; i++) {
-        if (phi[i] == i) {
-            for (int j = i; j < MAXN; j += i) {
-                phi[j] -= phi[j] / i;
-            }
-        }
+vector<int> totient_sieve(int n) {
+  vector<int> phi(n);
+  for (int i = 0; i < n; i++) phi[i] = i;
+  for (int i = 2; i < n; i++) {
+    if (phi[i] == i) {
+      for (int j = i; j < n; j += i) {
+        phi[j] -= phi[j] / i;
+      }
     }
+  }
+  return phi;
 }
 
+vector<int> phi_1_to_n(int n) {
+  vector<int> phi(n + 1);
+  phi[0] = 0;
+  phi[1] = 1;
+  for (int i = 2; i <= n; i++)
+    phi[i] = i - 1;
+  for (int i = 2; i <= n; i++)
+    for (int j = 2 * i; j <= n; j += i)
+      phi[j] -= phi[i];
+
+  return phi;
+}
 
 
 // To get the value of phi(x) for L <= x <= R, use phi[x - L].
