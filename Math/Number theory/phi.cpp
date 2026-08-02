4f59b9c78434359b9b418aad2f5bf161f046ff38
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-02 06:58:57 +0300

    Add segmented sieve for calculating phi function
    Implement segmented sieve algorithm to compute phi values in a range.

--- Math/Number theory/phi.cpp
+++ Math/Number theory/phi.cpp
@@ -25,3 +25,40 @@ void totient_sieve() {
         }
     }
 }
+
+
+
+// To get the value of phi(x) for L <= x <= R, use phi[x - L].
+const int MAX_RANGE = 1e6 + 6;
+vector<int> primes;
+int phi[MAX_RANGE], rem[MAX_RANGE];
+
+vector<int> linear_sieve(int n) { 
+  vector<bool> composite(n + 1, 0);
+  vector<int> prime;
+  // 0 and 1 are not composite (nor prime)
+  composite[0] = composite[1] = 1;
+  for(int i = 2; i <= n; i++) {
+    if(!composite[i]) prime.push_back(i);
+    for(int j = 0; j < prime.size() && i * prime[j] <= n; j++) {
+      composite[i * prime[j]] = true;
+      if(i % prime[j] == 0) break;
+    }
+  }
+  return prime;
+}
+void segmented_phi(int L, int R) { 
+  for(int i = L; i <= R; i++) {
+    rem[i - L] = i;
+    phi[i - L] = i;
+  }
+  for(int i : primes) {
+    for(int j = max(i * i, (L + i - 1) / i * i); j <= R; j += i) {
+      phi[j - L] -= phi[j - L] / i;
+      while(rem[j - L] % i == 0) rem[j - L] /= i;
+    }
+  }
+  for(int i = 0; i < R - L + 1; i++) {
+    if(rem[i] > 1) phi[i] -= phi[i] / rem[i];
+  }
+}
