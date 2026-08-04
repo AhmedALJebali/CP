bfa273413bc8423a16abe97874f2869f3eb4940b
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-04 14:12:28 +0300

    Update get_not_coprimes.cpp

--- Math/Number theory/get_not_coprimes.cpp
+++ Math/Number theory/get_not_coprimes.cpp
@@ -31,3 +31,57 @@ int countSubseqGcdOne(const vector<int>& a) {
   }
   return dp[1]%MOD;
 }
+
+int countSubseqGcdOne(vector<int>& a) {
+  int n=a.size();
+  int MAXA = *max_element(all(a));
+  vector<int> freq(MAXA + 1, 0);
+  for (int v : a)
+    ++freq[v];
+  // ---------- Linear sieve for Möbius ----------
+  vector<int> mu(MAXA + 1);
+  vector<int> lp(MAXA + 1, 0);
+  vector<int> primes;
+  mu[1] = 1;
+  for (int i = 2; i <= MAXA; i++) {
+    if (lp[i] == 0) {
+      lp[i] = i;
+      primes.push_back(i);
+      mu[i] = -1;
+    }
+    for (int p : primes) {
+      int v = 1LL * p * i;
+      if (v > MAXA) break;
+      lp[v] = p;
+      if (p == lp[i]) {
+        mu[v] = 0;
+        break;
+      } else {
+        mu[v] = -mu[i];
+      }
+    }
+  }
+  // ---------- Count multiples ----------
+  vector<int> cnt(MAXA + 1, 0);
+  for (int d = 1; d <= MAXA; d++) {
+    for (int m = d; m <= MAXA; m += d)
+      cnt[d] += freq[m];
+  }
+  // ---------- Powers of two ----------
+  vector<int> pw(n + 1);
+  pw[0] = 1;
+  for (int i = 1; i <= n; i++)
+    pw[i] = (pw[i - 1] * 2LL) % MOD;
+  int ans = 0;
+  for (int d = 1; d <= MAXA; d++) {
+    if (mu[d] == 0) continue;
+    int ways = pw[cnt[d]] - 1;
+    if (ways < 0) ways += MOD;
+    ans += 1LL * mu[d] * ways;
+    ans %= MOD;
+  }
+  ans %= MOD;
+  if (ans < 0) ans += MOD;
+  return ans;
+}
+
