fe994f12477566eb84853658dcd288d5a9bba542
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-04 15:27:14 +0300

    Update PST.cpp

--- Data structure/Segment Tree/PST.cpp
+++ Data structure/Segment Tree/PST.cpp
@@ -42,4 +42,33 @@ int get_kth(Node* root, int k, int lx = 0, int rx = N) {
         return get_kth(root->r, k - left_sum, mid, rx);
     }
 }
-
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    vector<Node*> ver(n + 2, nullptr);
+    vector<int> nxt(N, 0);
+    for (int i = n; i >= 1; i--) {
+        Node* cur = ver[i + 1];
+        if (nxt[a[i]] != 0) {
+            cur = upd(cur, nxt[a[i]], -1);
+        }
+        cur = upd(cur, i, 1);
+        ver[i] = cur;
+        nxt[a[i]] = i;
+    }
+    for (int k = 1; k <= n; k++) {
+        int ans = 0;
+        int L = 1;
+        while (L <= n) {
+            ans++;
+            L = get_kth(ver[L], k + 1);
+        }
+        cout << ans << " ";
+    }
+    cout << "\n";
+}
+ 
