d04f02d2ff4531d5e9cdef8b886f8945192e815c
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-04 11:08:19 +0300

    Add get_coprimes function for coprime calculation
    Implement get_coprimes function to calculate coprime counts.

---
+++ Math/Number theory/get_coprimes.cpp
@@ -0,0 +1,21 @@
+int get_coprimes(int x, vector<int> &mul) {
+  vector<int> p;
+  while (x > 1) {
+    int prime = spf[x];
+    p.push_back(prime);
+    while (x % prime == 0) {
+      x /= prime;
+    }
+  }
+  int nc = 0;
+  int msk = 1LL << p.size();
+  for (int j = 1; j < msk; j++) {
+    int lcm = 1;
+    for (int idx = 0; idx < p.size(); idx++) {
+      if (j >> idx & 1) lcm *= p[idx];
+    }
+    if (__builtin_popcountll(j) & 1) nc += mul[lcm];
+    else nc -= mul[lcm];
+  }
+  return nc;
+}
