57c6f175177a11c3ace3700f35fad573c56a2845
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-05 15:13:44 +0300

    Implement point-in-triangle check
    Add function to determine if a point is inside a triangle.

--- Geometry/Trigonometry.cpp
+++ Geometry/Trigonometry.cpp
@@ -150,3 +150,12 @@ T getRegularPyramidVolume(int n, T s) {
 
     return getPyramidVolume(baseArea, H);
 }
+int is_point_in_triangle(pt a, pt b, pt c, pt p) {
+    if (sgn(cross(b - a,c - a)) < 0) swap(b, c);
+    int c1 = sgn(cross(b - a,p - a));
+    int c2 = sgn(cross(c - b,p - b));
+    int c3 = sgn(cross(a - c,p - c));
+    if (c1<0 || c2<0 || c3 < 0) return 1;
+    if (c1 + c2 + c3 != 3) return 0;
+    return -1;
+}
