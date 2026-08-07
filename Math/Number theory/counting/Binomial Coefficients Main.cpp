99932c9fc4b620a8b57b11fc37bddcd023067073
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-07 23:07:44 +0300

    Implement derangement calculation function
    Added a function to calculate the number of derangements for 'n' items.

--- Math/Number theory/counting/Binomial Coefficients Main.cpp
+++ Math/Number theory/counting/Binomial Coefficients Main.cpp
@@ -125,3 +125,16 @@ int stars_and_bars_bounded(int n, int k, int x, int y) {
 int count_Subseq_Sum_Equal_S(int s) {
   return modPow(2,s-1);
 }
+
+
+// Returns the number of derangements of 'n' items (permutations where no element remains in its original position).
+int getDerangement(int n) {
+  if (n == 0) return 1;
+  if (n == 1) return 0;
+  int d0 = 1, d1 = 0,ret = 0;
+  for (int i = 2; i <= n; i++) {
+    ret = (i - 1) * (d1 + d0);
+    d0 = d1 , d1 = ret;
+  }
+  return ret;
+}
