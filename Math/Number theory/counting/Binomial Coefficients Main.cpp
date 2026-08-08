f88996b71b8d965564906b01b58a7f705de2657b
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-08 15:44:09 +0300

    Update Binomial Coefficients Main.cpp

--- Math/Number theory/counting/Binomial Coefficients Main.cpp
+++ Math/Number theory/counting/Binomial Coefficients Main.cpp
@@ -1,12 +1,6 @@
-// ===================== CONSTANTS =====================
-const int N = 1e6 + 5; // Max n for factorials/modulo
-const int MOD = 1e9 + 7; // Modulo for contests
-
-
-// ===================== FACTORIALS & INVERSE =====================
+const int N = 1e6 + 5; 
+const int MOD = 1e9 + 7;
 int fact[N], inv_fact[N];
-
-// Fast exponentiation modulo m
 int modPow(int a, int e, int m = MOD) {
     int res = 1;
     a %= m;
@@ -20,7 +14,6 @@ int modPow(int a, int e, int m = MOD) {
 int modInverse(int n) {
     return modPow(n, MOD - 2);
 }
-// Precompute factorials and inverse factorials modulo MOD
 void preFactorial() {
     fact[0] = 1;
     for (int i = 1; i < N; i++)
@@ -29,18 +22,15 @@ void preFactorial() {
     for (int i = N - 2; i >= 0; i--)
         inv_fact[i] = 1LL * inv_fact[i + 1] * (i + 1) % MOD;
 }
-
-// nCr modulo MOD (O(1) using precomputed factorials)
 int nCrMod(int n, int r) {
     if (r < 0 || r > n) return 0;
     return 1LL * fact[n] * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
 }
-
-// nPr modulo MOD
 int nPrMod(int n, int r) {
     if (r < 0 || r > n) return 0;
     return 1LL * fact[n] * inv_fact[n - r] % MOD;
 }
+
 int nCr_large_n(int n, int r) {
   if (r < 0 || r > n) return 0;
   int num = 1;
