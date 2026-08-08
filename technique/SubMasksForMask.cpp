1c65503aebb43194083d2e33061ced90eb7c1aaa
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-08 20:56:30 +0300

    Implement SubMasksForMask function to iterate submasks

---
+++ technique/SubMasksForMask.cpp
@@ -0,0 +1,7 @@
+void SubMasksForMask(int mask){
+    //vis sub masks of the mask in descending order
+    for (int s=mask; s; s=(s-1)&mask){
+        //do your work on s
+    }
+    // handle the empty set here
+}
