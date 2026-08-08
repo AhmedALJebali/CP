4a8045b2eb92b86d42f15a9fc050fcdb4a5fd826
‪Ahmed Al-Jebali‬‏ <ahmedaljebalii@gmail.com>
2026-08-08 20:50:23 +0300

    Refactor countSetBits function to use int type

---
+++ technique/countSetBits.cpp
@@ -0,0 +1,15 @@
+// Function to calculate the total sum of set bits from 1 to n
+int countSetBits(long long n) {
+    if (n <= 0) {
+        return 0;
+    }
+    int x = 0;
+    while ((1LL << x) <= n) {
+        x++;
+    }
+    x--; 
+    int bits_up_to_power = x * (1LL << (x - 1));
+    int msbs_from_power_to_n = n - (1LL << x) + 1;
+    int rest = countSetBits(n - (1LL << x));
+    return bits_up_to_power + msbs_from_power_to_n + rest;
+}
